4ea315521714a9f2ef8adf9660e7112f1643fa20
Jackson Tian <shyvo1987@gmail.com>
2015-07-21 13:34:37 +0800

    采用protobufjs模块

--- test/client.test.js
+++ test/client.test.js
@@ -2,6 +2,7 @@
 
 var expect = require('expect.js');
 var ots = require('../lib/client');
+var client = require('./common');
 
 describe('ots', function () {
   it('should ok', function* () {
@@ -13,7 +14,7 @@ describe('ots', function () {
     });
 
     try {
-      yield client.listTable();
+      yield* client.listTable();
     } catch (e) {
       expect(e.name).to.be('OTSAuthFailedError');
       expect(e.message).to.be('The AccessKeyID is disabled.');
@@ -31,12 +32,25 @@ describe('ots', function () {
     });
 
     try {
-      yield client.listTable();
+      yield* client.listTable();
     } catch (e) {
       expect(e.name).to.be('OTSAuthFailedError');
       expect(e.message).to.be('The AccessKeyID does not exist.');
       return;
     }
     expect(false).to.be.ok();
+  });
+
+  it('should ok with tables', function* () {
+    var response = yield* client.listTable();
+    expect(response.table_names).to.be.ok();
+    expect(response.table_names.length).to.be.above(0);
+  });
+
+  it('describeTable should ok', function* () {
+    var response = yield* client.describeTable('metrics');
+    expect(response).to.be.ok();
+    expect(response.table_meta.table_name).to.be('metrics');
+    expect(response.reserved_throughput_details).to.be.ok();
   });
 });
---
+++ test/common.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var ots = require('../lib/client');
+
+var options = {
+  accessKeyID: '<YOUR ACCESSKEYID>',
+  accessKeySecret: '<YOUR ACCESSKEYSECRET>',
+  instance: '<YOUR INSTANCE>',
+  region: '<YOUR REGION>'
+};
+
+try { // 覆盖
+  options = require('./config');
+} catch (ex) {}
+
+module.exports = ots.createClient(options);
